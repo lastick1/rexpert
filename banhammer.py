faec497a82f7c139e9c10e51d82f200d3a94e1a5
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2017-04-27 19:23:00 +0500

    ban hammer start

--- banhammer.py
+++ banhammer.py
@@ -1,2 +1,12 @@
+from pathlib import Path
+permanent_file = Path("./configs/ban_list_permanent")
+
+
 class BanHammer:
-    pass
+    def __init__(self):
+        self.banned = dict()
+        with permanent_file.open() as f:
+            rows = f.readlines()
+            for row in rows:
+                s = row.split(';')
+                self.banned[s[0]] = s[1]
