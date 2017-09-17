6ccf585d5e1fb34998089ece623396d3d19523f7
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2017-09-17 10:20:25 +0500

    helpers compile log

---
+++ helpers.py
@@ -0,0 +1,17 @@
+""" Полезные методы """
+from pathlib import Path
+import os
+
+# compile_log('./tmp', 'missionReport*.txt', './tmp/compiled')
+def compile_log(folder: str, report: str, dest: str):
+    """ Собрать лог по порядку из папки в один файл
+    :param folder: Папка
+    :param report: паттерн имени лога, который собрать
+    :param dest: целевой файл
+    """
+    folder = Path(folder)
+    with Path(dest).open(mode='w') as destination:
+        files = list(str(x) for x in folder.glob(report))
+        for file in sorted(files, key=os.path.getmtime):
+            with Path(file).open() as stream:
+                destination.writelines(stream.readlines())
