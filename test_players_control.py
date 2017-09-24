44a33e9b493d99ce93a693b50eed5827bffc5cf1
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2017-09-24 16:05:26 +0500

    #progress disconnect test offline status, refactoring

--- test_players_control.py
+++ test_players_control.py
@@ -4,7 +4,7 @@ import datetime
 import pathlib
 import pymongo
 from processing import PlayersController, Player
-from processing.player import ID, NICKNAME, KNOWN_NICKNAMES
+from processing.player import ID, NICKNAME, KNOWN_NICKNAMES, ONLINE
 from processing.objects import Aircraft, BotPilot
 from configs.objects import Objects
 from tests import mocks
@@ -15,6 +15,7 @@ DB_NAME = 'test_rexpert'
 TEST_NICKNAME = '_test_nickname'
 TEST_ACCOUNT_ID = '_test_id1'
 TEST_PROFILE_ID = '_test_profile_id1'
+TEST_PLAYER = Player.create_document(TEST_ACCOUNT_ID)
 FILTER = {ID: TEST_ACCOUNT_ID}
 
 class TestPlayersController(unittest.TestCase):
@@ -27,17 +28,35 @@ class TestPlayersController(unittest.TestCase):
         self.console_mock = ConsoleMock()
         self.controller = PlayersController(False, self.console_mock, self.players, self.squads)
         self.objects = Objects()
-        self._player = Player.create_document(TEST_ACCOUNT_ID)
 
     def tearDown(self):
         self.console_mock.socket.close()
         self.mongo.drop_database(DB_NAME)
         self.mongo.close()
 
+    def test_connect_player_init(self):
+        "Инициализируется игрок на первом входе на сервер"
+        # Act
+        self.controller.connect_player(TEST_ACCOUNT_ID, TEST_PROFILE_ID)
+        document = self.players.find_one(FILTER)
+        # Assert
+        self.assertNotEqual(None, document)
+
+    def test_connect_player_check_ban(self):
+        "Отправляется команда бана забаненого пользователя через консоль"
+        # Arrange
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
+        date = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.players.update_one(FILTER, update={'$set': {'ban_expire_date': date}})
+        # Act
+        self.controller.connect_player(TEST_ACCOUNT_ID, TEST_PROFILE_ID)
+        # Assert
+        self.assertIn(TEST_ACCOUNT_ID, self.console_mock.banned)
+
     def test_player_initialization(self):
         "Обновляется ник игрока на спауне"
         # Arrange
-        self.players.update_one(FILTER, {'$set': self._player}, upsert=True)
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
         aircraft = Aircraft(1, self.objects['I-16 type 24'], 201, 2, 'Test I-16')
         bot = BotPilot(2, self.objects['BotPilot'], aircraft, 201, 2, 'Test pilot')
         # Act
@@ -51,7 +70,7 @@ class TestPlayersController(unittest.TestCase):
     def test_spawn_player(self):
         "Отправляется приветственное сообщение игроку на спауне"
         # Arrange
-        self.players.update_one(FILTER, {'$set': self._player}, upsert=True)
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
         aircraft = Aircraft(1, self.objects['I-16 type 24'], 201, 2, 'Test I-16')
         bot = BotPilot(2, self.objects['BotPilot'], aircraft, 201, 2, 'Test pilot')
         # Act
@@ -63,29 +82,10 @@ class TestPlayersController(unittest.TestCase):
             (TEST_ACCOUNT_ID, 'Hello {}!'.format(TEST_NICKNAME)),
             self.console_mock.recieved_private_messages)
 
-    def test_connect_player_init(self):
-        "Инициализируется игрок на первом входе на сервер"
-        # Act
-        self.controller.connect_player(TEST_ACCOUNT_ID, TEST_PROFILE_ID)
-        document = self.players.find_one(FILTER)
-        # Assert
-        self.assertNotEqual(None, document)
-
-    def test_connect_player_check_ban(self):
-        "Отправляется команда бана забаненого пользователя через консоль"
-        # Arrange
-        self.players.update_one(FILTER, {'$set': self._player}, upsert=True)
-        date = datetime.datetime.now() + datetime.timedelta(days=1)
-        self.players.update_one(FILTER, update={'$set': {'ban_expire_date': date}})
-        # Act
-        self.controller.connect_player(TEST_ACCOUNT_ID, TEST_PROFILE_ID)
-        # Assert
-        self.assertIn(TEST_ACCOUNT_ID, self.console_mock.banned)
-
     def test_multiple_spawn_nickname(self):
-        "Неоднократный спаун не добавляет лишний ник в массив известных ников"
+        "Не добавляетcя лишний известный ник при неоднократный спауне"
         # Arrange
-        self.players.update_one(FILTER, {'$set': self._player}, upsert=True)
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
         aircraft = Aircraft(1, self.objects['I-16 type 24'], 201, 2, 'Test I-16')
         bot = BotPilot(2, self.objects['BotPilot'], aircraft, 201, 2, 'Test pilot')
         # Act
@@ -102,7 +102,7 @@ class TestPlayersController(unittest.TestCase):
     def test_multiple_spawn_new_nick(self):
         "Пополняются известные ники при спауне с новым ником"
         # Arrange
-        self.players.update_one(FILTER, {'$set': self._player}, upsert=True)
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
         aircraft = Aircraft(1, self.objects['I-16 type 24'], 201, 2, 'Test I-16')
         bot = BotPilot(2, self.objects['BotPilot'], aircraft, 201, 2, 'Test pilot')
         # Act
@@ -117,7 +117,14 @@ class TestPlayersController(unittest.TestCase):
         self.assertEqual([TEST_NICKNAME], document[KNOWN_NICKNAMES])
 
     def test_disconnect_player(self):
-        pass
+        "Ставится статус offline при дисконнекте игрока"
+        # Arrange
+        self.players.update_one(FILTER, {'$set': TEST_PLAYER}, upsert=True)
+        # Act
+        self.controller.disconnect_player(TEST_ACCOUNT_ID, TEST_PROFILE_ID)
+        # Assert
+        document = self.players.find_one(FILTER)
+        self.assertEqual(False, document[ONLINE])
 
 
 if __name__ == '__main__':
