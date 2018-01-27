73a8467039f055856c6c84a091b3b19627523330
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2018-01-27 22:05:12 +0500

    #refactoring

--- processing/division_control.py
+++ processing/division_control.py
@@ -1,6 +1,9 @@
 """Управление дивизиями"""
 import re
 
+import configs
+import processing
+
 from .division import Division, DIVISIONS
 from .campaign_mission import CampaignMission
 
@@ -19,15 +22,25 @@ class DivisionsController:
     def __init__(self, ioc):
         self._ioc = ioc
 
+    @property
+    def config(self) -> configs.Config:
+        """Конфигурация приложения"""
+        return self._ioc.config
+
+    @property
+    def storage(self) -> processing.Storage:
+        """Объект для работы с БД"""
+        return self._ioc.storage
+
     def initialize_divisions(self, tvd_name: str):
         """Инициализировать дивизии в кампании для указанного ТВД"""
         for name in DIVISIONS:
-            self._ioc.storage.divisions.update(
+            self.storage.divisions.update(
                 Division(
                     tvd_name=tvd_name,
                     name=name,
                     units=DIVISIONS[name],
-                    pos=self._ioc.config.mgen.cfg[tvd_name]['division_start_locations'][name]  # {'x': 0.0, 'z': 0.0}
+                    pos=self.config.mgen.cfg[tvd_name]['division_start_locations'][name]  # {'x': 0.0, 'z': 0.0}
                 )
             )
 
@@ -36,28 +49,28 @@ class DivisionsController:
         campaign_mission = _to_campaign_mission(self._ioc.campaign_controller.mission)
         for server_input in campaign_mission.server_inputs:
             if DIVISION_INPUT_RE.match(server_input['name']):
-                division = self._ioc.storage.divisions.load_by_name(campaign_mission.guimap, server_input['name'])
+                division = self.storage.divisions.load_by_name(campaign_mission.guimap, server_input['name'])
                 division.pos = server_input['pos']
-                self._ioc.storage.divisions.update(division)
+                self.storage.divisions.update(division)
 
     def damage_division(self, tvd_name: str, unit_name: str):
         """Зачесть уничтожение подразделения дивизии"""
         division_name = unit_name.split(sep='_')[1]
-        division = self._ioc.storage.divisions.load_by_name(tvd_name, division_name)
+        division = self.storage.divisions.load_by_name(tvd_name, division_name)
         division.units -= 1
         if division.units < 0:
             division.units = 0
-        self._ioc.storage.divisions.update(division)
+        self.storage.divisions.update(division)
 
     def repair_rate(self, penalties: int):
         """Получить множитель восстановления с учётом уничтоженных складов"""
-        result = 1 + (self._ioc.config.gameplay.division_repair - penalties * 5) / 100
+        result = 1 + (self.config.gameplay.division_repair - penalties * 5) / 100
         return result if result > 1 else 1
 
     def repair_division(self, tvd_name: str, division_name: str, penalties: int):
         """Восполнить дивизию"""
-        division = self._ioc.storage.divisions.load_by_name(tvd_name, division_name)
+        division = self.storage.divisions.load_by_name(tvd_name, division_name)
         division.units *= self.repair_rate(penalties)
         if division.units > DIVISIONS[division_name]:
             division.units = DIVISIONS[division_name]
-        self._ioc.storage.divisions.update(division)
+        self.storage.divisions.update(division)
