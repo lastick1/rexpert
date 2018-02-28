187a8ef40687664b966d9a0ab63e2448b3bcd267
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2018-02-28 18:51:41 +0500

    #buildfix

--- processing/warehouses_selector.py
+++ processing/warehouses_selector.py
@@ -3,6 +3,7 @@
 В миссии у сторон по 2 склада
 Склады выбираются после аэродромов и на удалении от них и дивизий
 """
+import logging
 import random
 
 import geometry
@@ -17,7 +18,9 @@ class WarehousesSelector:
         self._current = current
 
     def _get_max_deaths(self, country: int):
-        return max(x.deaths for x in self._warehouses if x.country == country) + 1
+        if self._warehouses:
+            return max(x.deaths for x in self._warehouses if x.country == country) + 1
+        return 0
 
     def select(self, tvd: model.Tvd) -> list:
         """Выбрать склады для ТВД"""
@@ -32,6 +35,9 @@ class WarehousesSelector:
                           if x.deaths < max_deaths
                           and x.country == country
                           and tvd.is_rear(x, country))
+        if not warehouses:
+            logging.warning(f'No warehouses selected {country}')
+            return list()
         result = list(x for x in self._current
                       if x.deaths < max_deaths
                       and x.country == country
