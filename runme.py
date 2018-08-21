e9ee972de349ff34695cf58888e5363d85199220
Maxim Ikonnikov <lastick1@gmail.com>
2018-08-21 13:12:55 +0500

    start from command line #resolved

--- runme.py
+++ runme.py
@@ -15,11 +15,44 @@ where <command> is one of:
 
 runme.py help <command>"""
 
+GENERATE_HELP = """generate usage: runme.py generate [attack] <mission_name> <tvd_name> <date>
+    [<attacking_country> <attacked_airfield_name>]
+
+where
+    mission_name is one of: result1, result2
+    tvd_name is one of: moscow, stalingrad, kuban
+    date is mission date in format dd.MM.yyyy
+    attack is an option to generate attacking mission type with required parameters:
+        attacking_country is one of: 101, 201
+        attacked_airfield_name is the airfield name from moscow_fields.csv or stalin_fields.csv or kuban_fields.csv"""
+
+RUN_HELP = """run usage: runme.py run
+    program start reading game log files, produced by dserver.exe and begin controlling campaign"""
+
+COMPILE_HELP = """compile usage: runme.py compile <task>
+
+where task is one of: gif, ldf, log
+    gif: program compiles map thumbnails from current/map_thumbnails directory into gif animation file
+    ldf: program compiles separate location database files into one large
+    log: program compiles game text log file from ./tmp directory into one large"""
+
+RESET_HELP = """reset usage: runme.py reset
+    program resets campaign and players progress to initial values"""
+
+INITIALIZE_HELP = """initialize usage: runme.py initialize
+    program creates required database, files and prepares all to start up the server"""
+
 
 def show_help(command: str):
     """Вывести помощь в консоль"""
     if command == 'run':
-        print()
+        print(RUN_HELP)
+    elif command == 'compile':
+        print(COMPILE_HELP)
+    elif command == 'reset':
+        print(RESET_HELP)
+    elif command == 'initialize':
+        print(INITIALIZE_HELP)
     else:
         print(MAIN_HELP)
 
@@ -36,6 +69,7 @@ def compile_gif():
 
 def compile_ldf():
     """Собрать декомпозировнаные базы локаций в базы локаций для покраски"""
+    # TODO добавить аргумент: ТВД
     for tvd_name in ('moscow', ):
         utils.compile_ldf(f'./data/ldf/{tvd_name}',
                           f'./data/ldf/{tvd_name}_base.ldf')
@@ -43,6 +77,19 @@ def compile_ldf():
 
 def _compile(args: list):
     """Обработка команды компиляции gif/лога/ldf"""
+    _args_count = len(args)
+    if _args_count > 0:
+        _command = args[0]
+        if _command == 'ldf':
+            compile_ldf()
+        elif _command == 'gif':
+            compile_gif()
+        elif _command == 'log':
+            compile_log()
+        else:
+            print(COMPILE_HELP)
+    else:
+        print(COMPILE_HELP)
 
 
 def reset():
@@ -57,19 +104,6 @@ def initialize_campaign():
     controller = processing.CampaignController(
         dependency_container.DependencyContainer())
     controller.initialize()
-
-
-GENERATE_HELP = """generate usage: runme.py generate [attack] <mission_name> <tvd_name> <date>
-    [<attacking_country> <attacked_airfield_name>]
-
-where
-    mission_name is one of: result1, result2
-    tvd_name is one of: moscow, stalingrad, kuban
-    date is mission date in format dd.MM.yyyy
-    attack is an option to generate attacking mission type with required parameters:
-        attacking_country is one of: 101, 201
-        attacked_airfield_name is the airfield name from moscow_fields.csv or stalin_fields.csv or kuban_fields.csv
-"""
 
 
 def generate(name: str, tvd_name: str, date: str, attacking_country=0, attacked_airfield_name: str = None):
@@ -100,27 +134,14 @@ def run():
     core.LogsReader(dependency_container.DependencyContainer()).start()
 
 
-logging.basicConfig(
-    format=u'%(asctime)s %(levelname)-8s %(filename)-40s:%(lineno)-3d # %(message)s', level=logging.DEBUG)
-logging.getLogger('PIL').setLevel(logging.WARNING)
-logging.info("Program Start.")
-# compile_log()  # объединить лог в один файл
-# compile_gif()  # собрать картинки в гифку
-# compile_ldf()  # объединить файлы баз локаций
-
-# reset()  # обнуление состояния кампании
-# initialize_campaign()
-# генерация атаки
-# generate('result1', 'moscow', '01.09.1941', attacking_country=201, attacked_airfield_name='vatulino')
-# генерация противостояния
-# generate('result1', 'moscow', '01.09.1941')
-# run()
-
-
 def main(args: list):
     """Основная точка входа в приложение"""
     _args_count = len(args)
     if _args_count > 0:
+        logging.basicConfig(
+            format=u'%(asctime)s %(levelname)-8s %(filename)-40s:%(lineno)-3d # %(message)s', level=logging.DEBUG)
+        logging.getLogger('PIL').setLevel(logging.WARNING)
+        logging.info("Program Start.")
         _command = args[1].lower()
         if _command == 'run':
             run()
