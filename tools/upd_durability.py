be95d1f3a7efc54d3fc38bfb7e8e98dadc0e0665
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2018-12-08 19:11:29 +0500

    feat(tools): добавить скрипт обновления прочности у статики

---
+++ tools/upd_durability.py
@@ -0,0 +1,50 @@
+import csv
+import sys
+import glob
+
+
+def get_models_durability():
+    models_durability = {}
+    with open('durability.csv') as f:
+        reader = csv.reader(f, delimiter=';')
+        for row in reader:
+            models_durability[row[0]] = int(row[1])
+    return models_durability
+
+
+def update_durability(group_file):
+    models_durability = get_models_durability()
+    lines = []
+    next_durability = 0
+    wait_durability = False
+    with open(group_file) as f:
+        for line in f.readlines():
+            if line.strip().startswith('Model ='):
+                lines.append(line)
+                for model, durability in models_durability.items():
+                    if model in line:
+                        wait_durability = True
+                        next_durability = durability
+                        break
+            elif wait_durability and line.strip().startswith('Durability ='):
+                wait_durability = False
+                line_parts = line.split(' = ')
+                new_line = '{field} = {value};\n'.format(field=line_parts[0], value=next_durability)
+                lines.append(new_line)
+            else:
+                lines.append(line)
+
+    with open(group_file, 'w') as f:
+        f.writelines(lines)
+
+
+if __name__ == '__main__':
+    try:
+        files = glob.glob('**', recursive=True)
+        for path in files:
+            if path.endswith('.Group'):
+                print(path)
+                update_durability(path)
+    except Exception as e:
+        print(e)
+        input('Press enter to close...')
