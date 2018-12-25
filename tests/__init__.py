8c801bc38d2f754489cf42526b26f227e73bc34c
m.ikonnikov <m.ikonnikov@infinnity.ru>
2018-12-25 13:28:30 +0500

    test(config): настроить nose для запуска тестов

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,3 @@
 # pylint: disable-all
-# from .test_events_control import TestIntegration
-# from .test_geometry import TestPoint, TestSegment
-# from .test_players_control import TestPlayersController
 from .mocks import ConsoleMock
 from .utils import clean_directory
