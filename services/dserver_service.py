0bc83efd8c316f2fbfe600e3fd689bf64c47b599
Maxim Ikonnikov <ikonnikov.maxim.vladimirovich@gmail.com>
2019-05-13 23:35:28 +0500

    feat: выводить в консоль команды rcon

--- services/dserver_service.py
+++ services/dserver_service.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 from typing import Dict, Any
 
+import logging
 
 from configs import Config
 from core import EventsEmitter
@@ -58,31 +59,47 @@ class DServerService(BaseEventService):
     def message_all(self, command: MessageAll) -> None:
         "Отправить сообщение всем игрокам"
         self._rcon.info_message(command.message)
+        if self._config.main.console_chat_output:
+            logging.info(f'CHAT:ALL:{command.message}')
 
     def message_allies(self, command: MessageAllies) -> None:
         "Отправить сообщение союзникам"
         self._rcon.allies_message(command.message)
+        if self._config.main.console_chat_output:
+            logging.info(f'CHAT:ALLIES:{command.message}')
 
     def message_axis(self, command: MessageAxis) -> None:
         "Отправить сообщение люфтваффе"
         self._rcon.axis_message(command.message)
+        if self._config.main.console_chat_output:
+            logging.info(f'CHAT:AXIS:{command.message}')
 
     def message_private(self, command: MessagePrivate) -> None:
         "Отправить сообщение игроку"
         self._rcon.private_message(command.account_id, command.message)
+        if self._config.main.console_chat_output:
+            logging.info(f'CHAT:{command.account_id}:{command.message}')
 
     def kick(self, command: PlayerKick) -> None:
         "Выбросить игрока с сервера"
         self._rcon.kick(command.account_id)
+        if self._config.main.console_cmd_output:
+            logging.info(f'KICK:{command.account_id}')
 
     def ban_short(self, command: PlayerBanP15M) -> None:
         "Забанить игрока на 15 минут"
         self._rcon.banuser(command.account_id)
+        if self._config.main.console_cmd_output:
+            logging.info(f'BANUSER:{command.account_id}')
 
     def ban_long(self, command: PlayerBanP7D) -> None:
         "Забанить игрока на 7 дней"
         self._rcon.ban(command.account_id)
+        if self._config.main.console_cmd_output:
+            logging.info(f'BAN:{command.account_id}')
 
     def server_input(self, command: ServerInput) -> None:
         "Активировать MCU ServerInput"
         self._rcon.server_input(command.name)
+        if self._config.main.console_cmd_output:
+            logging.info(f'SERVER_INPUT:{command.name}')
